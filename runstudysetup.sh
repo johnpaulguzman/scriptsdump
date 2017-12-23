b8ad9fd75247673a94782c7902e8b1b1a66ed598
JPG <japseow@gmail.com>
2017-12-23 21:54:36 +0800

    xmas

--- runstudysetup.sh
+++ runstudysetup.sh
@@ -5,13 +5,14 @@ vlc_time=$((60*0 + 60*0 + 00))
 #vlc_path= 
 gum_path='/home/jp/Repositories/OOM/main_ML.tex' 
 pdf_path='/home/jp/Repositories/OOM/main_ML.pdf' 
+pdf_page='37' 
+src_path='/home/jp/Youtube/Pleasus' 
 pdf_bookmark='nRandomVariable' 
-echo "jp" | sudo -S echo -e  "sudo activated \n${vlc_time} \n${vlc_path} \n${pdf_path}"
+echo "jp" | sudo -S echo -e  "sudo activated \ntime ${vlc_time} \nvlc ${vlc_path} \npdf ${pdf_path} \nsrc ${src_path}"
 sudo gummi ${gum_path} &
-firefox -new-tab &
-echo "file://${pdf_path}#${pdf_bookmark}" | xclip -sel clip &
-#nohup gedit &
+evince --page-label=${pdf_page} ${pdf_path} &
+pcmanfm -d ${src_path}
 #nohup vlc --start-time=${vlc_time} "${vlc_path}" &!
 #echo "TODO #####################################"
 #echo "Fix notes, then resume back to Statistical machine learning theory lectures."
-echo https://www.probabilitycourse.com/
+#echo https://www.probabilitycourse.com/
